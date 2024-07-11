0d6708421a57bed086153a1dc822d187cfdf8136
Kishanbhadoriya007 <kishanbhadoriya4123@gmail.com>
2024-07-11 14:55:42 +0530

    edits

--- helloworld2.cpp
+++ helloworld2.cpp
@@ -19,7 +19,17 @@ int main(){
 }
 
 void sort(int array[], int size ){
-    
+    int temp;
+    for(int i=1; i < size-1; i++ ){
+        for(int j=1; j < size-i-1; j++ ){  
+            if( array[j] > array[j+1]){
+                temp = array[j];
+                array [j] = array[j + 1];
+                array[j+1] = temp;
+            }
+
+    }
+    }
 }
 
 // double getTotal(double prices[], int size);
