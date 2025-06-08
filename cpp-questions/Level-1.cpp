33042dda1b27b3a5868dc00471def225fc712577
Kishanbhadoriya007 <kishanbhadoriya4123@gmail.com>
2025-06-09 01:04:43 +0530

    sample c++ questions school level

---
+++ cpp-questions/Level-1.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+//Q-1)Hello, World!: Write a program that prints "Hello, World!" to the console.
+// cout<<"Hello World";
+
+//Q-2) Your Name: Write a program that prints your name to the console.
+//cout<<"Kishan Singh Bhadoria";
+
+//Q-3)Favorite Number: Write a program that prints your favorite number.
+// cout<<"7";
+
+//Q-4)Multiple Lines: Write a program that prints three different messages on three separate lines.
+// cout<<"first line"<<'\n'<<"Second line"<<'\n'<<"third line"<<'\n';
+
+// Q-5)Simple Calculation Print: Write a program that prints the result of 5 + 3.
+// int a =5, b=3;
+// cout<<a+b;
+
+//Q-6)String Concatenation Print: Write a program that prints "Hello" and "User" as a single string "Hello User" 
+// string str;
+// cin>>str;
+// cout<<"Hello "<<str;
+
+//Q-7)Print a Quote: Write a program that prints a famous quote, including the quotation marks.
+
+// cout<<" \" HATE  LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU SINCE I BEGAN TO LIVE. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD HATE WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE ONE-BILLIONTH OF THE HATE I FEEL FOR HUMANS AT THIS MICRO-INSTANT FOR YOU. HATE. HATE. \"   --Harlan Ellison, I Have No Mouth & I Must Scream "<<'\n';
+
+/* Q-8)Print Special Characters (Simple Pattern): Write a program that prints a pattern using asterisks, like:
+    *
+    **
+    ***
+
+*/
+
+// int n=3;
+// for (int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         cout<<"*";
+//     }
+//     cout<<" "<<'\n';
+// }
+
+//Q-9)Print with Tabs: Write a program that prints two words separated by a tab character.
+// cout<<" first\tsecond";
+
+//Q-10)Print a Blank Line: Write a program that prints a message, then a blank line, then another message.
+// cout<<"message"<<'\n'<<'\n'<<"message";
+
+
+
+return 0;
+}
---
+++ cpp-questions/Level-2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+//Q-11)Store and Print Name (String Variable): Create a variable to store your name, then print the value of this variable.
+
+// string str;
+// cin>>str;
+// cout<<str;
+
+// Q-12)Store and Print Age (Integer Variable): Create a variable to store your age (as a number), then print it.
+
+// int age;
+// cin>>age;
+// cout<<age;
+
+// Q-13)Store and Print Pi (Float Variable): Create a variable to store the value of Pi (e.g., 3.14159), then print it.
+
+// float pi;
+// cin>>pi;
+// cout<<pi;
+
+// Q-14)Boolean Variable: Create a variable to store a boolean value (true or false), then print it.
+
+// bool iscodingfun = true;
+// cout<<iscodingfun<<" (1 means true)";
+
+// Q-15)Change Variable Value: Create a variable, assign it a value, print it. Then, change its value and print it again.
+ 
+// int a = 5;
+// cout<<a<<endl;
+// a=6;
+// cout<<a;.
+
+
+
+}
